e08e507f0fc80ebba2297438b5b98d504bc3d9cb
Francisco Andrade <francisco@inspr.com>
2021-05-18 18:26:10 -0300

    Dev: now returns error when creating booking with nonexistent user

--- flightex/lib/bookings/booking.ex
+++ flightex/lib/bookings/booking.ex
@@ -1,16 +1,24 @@
 defmodule Flightex.Bookings.Booking do
+  alias Flightex.Users.Agent, as: UserAgent
+
   @keys [:complete_date, :local_origin, :local_destination, :user_id, :id]
   @enforce_keys @keys
   defstruct @keys
 
   def build(complete_date, local_origin, local_destination, uid) do
-    {:ok,
-     %__MODULE__{
-       id: UUID.uuid4(),
-       complete_date: complete_date,
-       local_origin: local_origin,
-       local_destination: local_destination,
-       user_id: uid
-     }}
+    case UserAgent.get(uid) do
+      {:error, _msg} = error ->
+        error
+
+      {:ok, _user} ->
+        {:ok,
+         %__MODULE__{
+           id: UUID.uuid4(),
+           complete_date: complete_date,
+           local_origin: local_origin,
+           local_destination: local_destination,
+           user_id: uid
+         }}
+    end
   end
 end
