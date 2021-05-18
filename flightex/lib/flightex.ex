612215a594eec3a4276b83f2cffec948b0e45671
Francisco Andrade <francisco@inspr.com>
2021-05-17 22:08:48 -0300

    Dev: defined create/update delegations in Flightex

--- flightex/lib/flightex.ex
+++ flightex/lib/flightex.ex
@@ -1,18 +1,19 @@
 defmodule Flightex do
-  @moduledoc """
-  Documentation for `Flightex`.
-  """
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Bookings.Agent, as: BookingAgent
+  alias Flightex.Users.CreateOrUpdate, as: CorUUsers
+  alias Flightex.Bookings.CreateOrUpdate, as: CorUBookings
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Flightex.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_agents do
+    UserAgent.start_link(%{})
+    BookingAgent.start_link(%{})
   end
+
+  defdelegate create_or_update_user(params),
+    to: CorUUsers,
+    as: :call
+
+  defdelegate create_or_update_booking(params),
+    to: CorUBookings,
+    as: :call
 end
