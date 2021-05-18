41b0013a8a809e5658710157ce940c6700d48628
Francisco Andrade <francisco@inspr.com>
2021-05-18 18:27:12 -0300

    Fix: changed CreateOrUpdate call method params to be equal to the example given

--- flightex/lib/bookings/create_or_update.ex
+++ flightex/lib/bookings/create_or_update.ex
@@ -2,11 +2,10 @@ defmodule Flightex.Bookings.CreateOrUpdate do
   alias Flightex.Bookings.Agent, as: BookingAgent
   alias Flightex.Bookings.Booking
 
-  def call(%{
+  def call(uid, %{
         complete_date: complete_date,
         local_origin: local_origin,
-        local_destination: local_destination,
-        user_id: uid
+        local_destination: local_destination
       }) do
     get_date(complete_date)
     |> Booking.build(local_origin, local_destination, uid)
