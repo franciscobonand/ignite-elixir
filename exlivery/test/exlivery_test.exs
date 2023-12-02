16e5749939b8a829721a550b47d8687f0f0227aa
Francisco <bonand04@gmail.com>
2023-12-02 17:43:36 -0300

    reset

--- exlivery/test/exlivery_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule ExliveryTest do
-  use ExUnit.Case
-
-  # test "greets the world" do
-  #   assert Exlivery.hello() == :world
-
-  #   assert true == true
-  # end
-end
--- flightex/test/flightex_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule FlightexTest do
-  use ExUnit.Case
-end
