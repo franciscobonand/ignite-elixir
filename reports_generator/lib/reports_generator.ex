7ada7dfd917c16e436692818a7f12b14b5fe6ffc
Francisco <bonand04@gmail.com>
2023-12-24 17:13:53 -0300

    ReportsGenerator first version

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,18 +1,30 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @kinds ~w(users foods)
+
   def build(fname) do
     fname
     |> Parser.parse_file()
-    |> Enum.reduce(%{}, fn line, report -> sum_values(line, report) end)
+    |> Enum.reduce(%{"users" => %{}, "foods" => %{}}, fn line, report ->
+      sum_values(line, report)
+    end)
   end
 
-  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_, val} -> val end)
+  def fetch_higher_cost(report, kind) when kind in @kinds do
+    {:ok, Enum.max_by(report[kind], fn {_, val} -> val end)}
+  end
 
-  defp sum_values([id, _, price], report) do
-    Map.put(report, id, add_nums(report[id], price))
+  def fetch_higher_cost(_, _), do: {:error, "Invalid kind"}
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, add_nums(users[id], price))
+    foods = Map.put(foods, food_name, add_nums(foods[food_name], 1))
+    %{report | "users" => users, "foods" => foods}
   end
 
   defp add_nums(nil, n2), do: n2
   defp add_nums(n1, n2), do: n1 + n2
+
+  def print_kinds(), do: IO.inspect(@kinds)
 end
--- reports_generator/test/reports_generator_test.exs
+++ reports_generator/test/reports_generator_test.exs
@@ -1,8 +1,72 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
-  doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  describe "build/1" do
+    test "builds a report" do
+      fname = "report_test.csv"
+      response = ReportsGenerator.build(fname)
+
+      expected = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pizza" => 2
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "2" => 45,
+          "3" => 31,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert response == expected
+    end
+  end
+
+  describe "fetch_higher_cost/2" do
+    test "when kind is 'users', returns user that spent the most" do
+      fname = "report_test.csv"
+
+      response =
+        fname
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected = {:ok, {"5", 49}}
+      assert response == expected
+    end
+
+    test "when kind is 'foods', returns food that was most consumed" do
+      fname = "report_test.csv"
+
+      response =
+        fname
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected = {:ok, {"esfirra", 3}}
+      assert response == expected
+    end
+
+    test "when invalid kind is received" do
+      fname = "report_test.csv"
+
+      response =
+        fname
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("invalid")
+
+      expected = {:error, "Invalid kind"}
+      assert response == expected
+    end
   end
 end
