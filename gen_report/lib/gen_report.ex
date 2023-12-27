0a94a924ca796e9ad047bb3b47f785e4721ed33b
Francisco <bonand04@gmail.com>
2023-12-26 21:00:35 -0300

    GenReport file parser

---
+++ gen_report/lib/gen_report.ex
@@ -0,0 +1,5 @@
+defmodule GenReport do
+  def build do
+    # TO DO
+  end
+end
---
+++ gen_report/lib/parser.ex
@@ -0,0 +1,39 @@
+defmodule GenReport.Parser do
+  @months %{
+    "1" => "janeiro",
+    "2" => "fevereiro",
+    "3" => "março",
+    "4" => "abril",
+    "5" => "maio",
+    "6" => "junho",
+    "7" => "julho",
+    "8" => "agosto",
+    "9" => "setembro",
+    "10" => "outubro",
+    "11" => "novembro",
+    "12" => "dezembro"
+  }
+
+  def parse_file(fname) do
+    fname
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> format_data()
+  end
+
+  defp format_data([name, h, d, m, y]) do
+    [
+      String.downcase(name),
+      String.to_integer(h),
+      String.to_integer(d),
+      @months[m],
+      String.to_integer(y)
+    ]
+  end
+end
